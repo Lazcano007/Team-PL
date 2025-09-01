60aa91ba8153f6e8175f310dcf58f8d094b08357
Lazcano007 <Pedrolazcano@live.com>
2025-09-01 12:05:04 +0200

    feat: assign spin on new order

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import connectDB from "./database/db";
 import dotenv from 'dotenv';
+import { User } from './model/userInterface';
+import { Order } from './model/orderInterface';
+import { Spin } from './model/spinInterface';
+
 
 dotenv.config();
 
@@ -13,4 +17,30 @@ app.use(express.json());
 app.listen(PORT, async () => {
 //   await connectDB();
   console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.post("/order", async (req, res) => {
+   
+  const { id, userId } = req.body;
+  
+  if (!id || !userId) 
+    return res.status(400).json({ error: "id och userId krävs" });
+
+    let user = await User.findOne({ id: userId });
+  
+    if (!user) {
+        user = new User({ id: userId, name: `User ${userId}`, spins: 0 }); // Skapa en ny användare om den inte finns
+  
+      await user.save();
+    }
+
+    const order = new Order({ id, userId, createdAt: new Date() }); // Skapa en ny order 
+    
+    await order.save();
+
+    user.spins += 1; // Spinn tilldelas
+    
+    await user.save();
+
+    res.json({ message: "Bra jobbat, du fick ett spinn", spins: user.spins });
 });
