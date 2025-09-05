c09aee7f512a0395110dc10e1ab1789d1e9ee04b
Lara <lara.salman01@hotmail.com>
2025-09-05 15:58:15 +0200

    feat: add route to history page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import OrderPage from "./pages/OrderPage";
 import SpinWheel from "./pages/spinWheel";
+import HistoryPage from "./pages/HistoryPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/order" element={<OrderPage />} />
         <Route path="/spin" element={<SpinWheel />} />
+        <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </Router>
   );
