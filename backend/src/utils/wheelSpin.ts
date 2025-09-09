4b2f4e10ae025b395aded364b13ff49241004707
Lazcano007 <Pedrolazcano@live.com>
2025-09-09 09:52:14 +0200

    feat: added comments to wheelspin

--- backend/src/utils/wheelSpin.ts
+++ backend/src/utils/wheelSpin.ts
@@ -7,11 +7,11 @@ export const prizes = [
 ];
 
 export function wheelSpin(): number {
-  const totalChance = prizes.reduce((sum, p) => sum + p.chance, 0);
-  let rand = Math.random() * totalChance;
+  const totalChance = prizes.reduce((sum, p) => sum + p.chance, 0); // summerara alla chanser
+  let rand = Math.random() * totalChance;  // väljer en slumpmässig siffra mellan 0 och totalChange
   for (const prize of prizes) {
-    rand -= prize.chance;
+    rand -= prize.chance;  //Om rand blir mindre än 0, returneras det prisets belopp.
     if (rand < 0) return prize.amount;
   }
-  return prizes[0]?.amount ?? 0;
+  return prizes[0]?.amount ?? 0;  //Returnerar det första prisets belopp 
 }
